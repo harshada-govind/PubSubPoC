37c170161fb2901dfcb93c241d9b926ecd5bb7c2
Harshada Govind <harshada.govind@opusenergy.com>
2020-11-20 22:02:04 +0000

    Publisher Messages - Events or Commands

---
+++ Publisher.Messages/Events/SmartMeterRegistered.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NServiceBus;
+namespace Publisher.Messages.Events
+{
+    public class SmartMeterRegistered : IEvent
+    {
+        public string RegistrationId { get; set; }
+        public string Mpxn { get; set; }
+        public DateTime SupplyStartDate { get; set; }
+    }
+}
